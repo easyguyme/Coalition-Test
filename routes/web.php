8091cb594109219fc9c9c30d65926a17930cf8c6
easyguyme1 <imrank@paysokosystems.com>
2025-01-21 01:05:54 +0300

    feat(controller): Add products routes and controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\ProductController;
+
+Route::get('/', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products', [ProductController::class, 'getProducts']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
